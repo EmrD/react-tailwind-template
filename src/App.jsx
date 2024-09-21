5f31a31768552ba9b7f49700b96b45d93f18f122
emrd434 <emrd434@gmail.com>
2024-09-21 15:44:46 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+  return(
+    <>
+    <div class="bg-red-500 text-white font-bold text-2xl px-4 py-2 rounded-md">
+      Tailwind Template For React!
+    </div>
+    <details class="px-4 py-2">
+    <pre>
+      Start editing App.jsx to use Tailwind!
+    </pre>
+    </details>
+    </>
+  )
+}
+
+export default App;
